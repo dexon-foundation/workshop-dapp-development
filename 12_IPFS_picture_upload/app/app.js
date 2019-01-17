bcba94f53eb5970050e983a5bfa6c75431182d09
Chen Wei <wei.chen@cobinhood.com>
2019-01-18 02:19:05 +0800

    IPFS upload example for Cliff. With comparison of size in base64 and IPFS hash

--- 12_IPFS_picture_upload/app/app.js
+++ 12_IPFS_picture_upload/app/app.js
@@ -1,88 +1,33 @@
 console.log('WELCOME TO DEXON WORKSHOP');
+const fileReaderPullStream = require('pull-file-reader')
 
 const init = async () => {
-  /**
-   * Make sure that when you get here, basic UI has already been rendered.
-   * Web3 bundle is large so we might want to import it asynchronounsly
-   *
-   * Web3 team is working on reducing the bundle size, let's see how it goes
-   * https://github.com/ethereum/web3.js/pull/2000
-   */
-  const Web3 = await import('web3');
 
-  let httpHandler;
-  let wsHandler;
-  let netId;
-  let myAccount;
+  const ipfs = IpfsHttpClient({ 
+    host: 'ipfs.infura.io',
+    port: '5001',
+    protocol: 'https',
+   });
 
-  if (window.dexon) {
-    await window.dexon.enable();
-    httpHandler = new Web3.default(window.dexon);
-    netId = await httpHandler.eth.net.getId();
-    myAccount = (await httpHandler.eth.getAccounts())[0];
-  }
-
-  const getWebsocketEndpoint = () => {
-    const DEXON_WS_ENDPOINT = (location.protocol === 'https:')
-      ? 'wss://ws-proxy.dexon.org'
-      : 'ws://testnet.dexon.org:8546';
-
-    switch(netId) {
-      case 5777: // If DekuSan is using local rpc
-        return 'ws://localhost:8545';
-      // If DekuSan is connect to testnet or not availble
-      case 238:
-      default:
-        return DEXON_WS_ENDPOINT;
+  document.getElementById('upload').onchange = async ({ target }) => {
+    document.getElementById('ipfsHash').innerText = 'IPFS uploading...';
+    document.getElementById('base64').innerText = '';
+    const file = target.files[0];
+    const fileStream = fileReaderPullStream(file);
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      document.getElementById('preview').src = reader.result;
+      console.log(reader.result.length);
+      document.getElementById('base64').innerText = `Base64: ${reader.result.length} characters`;
     }
-  }
-  const ws_endpoint = getWebsocketEndpoint();
-  console.log(`Websocket endpoint: ${ws_endpoint}`);
 
-  wsHandler = new Web3.default(ws_endpoint);
-
-  const contractInfo = (await import('../build/contracts/Hello.json')).default;
-  const { abi, networks } = contractInfo;
-  // If there's no netId, we use 238 as default network
-  const address = networks[netId || 238].address;
-
-  let contractReader;
-  let contractWriter;
-
-  // contractReader is created from wsHandler
-  contractReader = new wsHandler.eth.Contract(abi, address);
-
-  // contractWriter is created from httpHandler
-  if (httpHandler) {
-    contractWriter = new httpHandler.eth.Contract(abi, address);
+    const t = await ipfs.add(fileStream, { progress: p => console.log(`progress ${p}`) });
+    console.log(t);
+    document.getElementById('ipfsHash').innerHTML = `IPFS Hash: <a href="https://gateway.ipfs.io/ipfs/${t[0].hash}">${t[0].hash}</a>`;
+    // console.log(file);
   }
 
-  // DOM Element to display "value" in contract
-  const valueDisplayElement = document.getElementById('value');
-  // Get current value and display it
-  const val = await contractReader.methods.value().call();
-  valueDisplayElement.textContent = val;
-
-  // Subscribe to "UpdateNumber" event in order to have "value" updated automatically
-  contractReader.events.UpdateNumber({}, (err, data) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    console.log('[Event] UpdateNumber', data.returnValues.value);
-    valueDisplayElement.textContent = data.returnValues.value;
-    
-  });
-
-  // Call "update" function in the contract when we click on the update button
-  const updateButton = document.getElementById('update');
-  updateButton.onclick = async () => {
-    if (contractWriter && myAccount) {
-      await contractWriter.methods.update().send({
-        from: myAccount,
-      });
-    }
-  }
 };
 
 init();
